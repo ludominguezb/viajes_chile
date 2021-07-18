a436d2e91b53a75e23b99e1d4fdcfb5b0bb96580
Luis <ludominguezb@gmail.com>
2021-07-18 19:24:20 -0400

    agregando typeform

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  $('.nav-link').click(function(event){
+    if (this.hash !=="") {
+      event.preventDefault();
+
+      var hash= this.hash;
+
+      $('html, body').animate({
+        scrollTop:$(hash).offset().top
+      });
+    }
+  });
+
+
+
+
+})
