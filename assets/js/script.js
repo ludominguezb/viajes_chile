0077a21004db95ba5abbbcb7fef579ebacf122a7
Luis <ludominguezb@gmail.com>
2021-07-18 21:42:47 -0400

    terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,33 @@ $(function(){
     }
   });
 
+  $( "#clickme1" ).click(function() {
+    $( "#cuerpo1" ).toggle( "800", function() {
+    });
+    });
 
+  $( "#clickme2" ).click(function() {
+    $( "#cuerpo2" ).toggle( "800", function() {
+    });
+    });
+
+    $( "#clickme3" ).click(function() {
+      $( "#cuerpo3" ).toggle( "800", function() {
+      });
+      });
+
+
+      $(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+          if (scroll >= 10) {
+             $("nav").addClass("cabeceraOscura");
+          }else{
+             $("nav").removeClass("cabeceraOscura");
+          }
+      });
+
+
+  $('[data-toggle="tooltip"]').tooltip()
 
 
 })
